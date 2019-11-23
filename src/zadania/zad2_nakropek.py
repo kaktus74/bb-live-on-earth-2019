a1313655ba89fde8e3a86cd965c57204cbd0e6b3
nakropek <nakropek@gmail.com>
2019-11-23 20:11:57 +0100

    Musze sie ogarnac

--- src/zadania/zad2_nakropek.py
+++ src/zadania/zad2_nakropek.py
@@ -1,10 +1,17 @@
 import random
 from random import randint
-def kostka (liczba_rzutow, liczba_scianek):
+def main ():
+    ilosc_scianek = (int ('Ile ścianek ma twoja kostka? '))
+    ilosc_rzutow = (int ('Ile razy chcesz rzucić? '))
     suma = 0
     for proba in range (0, liczba_rzutow):
         rzut = randint (1, liczba_scianek)
         print ('Wynik: ', rzut)
         suma = suma + rzut
     print ('Suma wyników to: ', suma)
+
+if __name__ == '__main__':
+    main ()
+
+    
     
