14e90085678ad145aed413fcab171b1469ed37c2
nakropek <nakropek@gmail.com>
2019-12-01 20:42:44 +0100

    zapisywanie do folderu data

--- src/zadania/zad3_nakropek.py
+++ src/zadania/zad3_nakropek.py
@@ -1,7 +1,9 @@
 import ephem
 import time
 from datetime import datetime, timezone
+from os.path import split
 
+my_dir = split (__file__) [0]
 t = datetime.now(timezone.utc)
 
 nazwa = 'ISS (ZARYA)'
@@ -9,7 +11,7 @@ nazwa = 'ISS (ZARYA)'
 linijka1='1 25544U 98067A   19328.47806978  .00001893  00000-0  41124-4 0  9999'
 linijka2='2 25544  51.6463 286.5827 0006227 320.1499 141.5168 15.50037910200122'
 
-ISS_pozycje = open ('ISS_pozycje_{0}.txt'.format(t.strftime("%d-%m-%Y-%H-%M")), 'w')
+ISS_pozycje = open ('{0}/../../data/ISS_pozycje_{1}.txt'.format(my_dir, t.strftime("%d-%m-%Y-%H-%M")), 'w')
 
 iss=ephem.readtle(nazwa,linijka1,linijka2)
 
